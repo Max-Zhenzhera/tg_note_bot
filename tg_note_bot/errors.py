8d4ad6817d82943fb3fee923e35a2743bd8deefc
Max Zhenzhera <megashurik@urk.net>
2021-04-11 17:10:22 +0300

    added new errors file for project scope errors

---
+++ tg_note_bot/errors.py
@@ -0,0 +1,11 @@
+"""
+Contains global errors.
+
+.. exception:: TgNoteBotError(Exception)
+"""
+
+
+class TgNoteBotError(Exception):
+    """
+    Implements error for entire project exceptions
+    """
