4154156d027edfab10dd70737bf50ee573b62640
Max Zhenzhera <megashurik@urk.net>
2021-04-29 15:14:46 +0300

    improved handling of errors on logging setup; refactored

--- tg_note_bot/utils/logging_/logging_.py
+++ tg_note_bot/utils/logging_/logging_.py
@@ -1,5 +1,10 @@
 """
 Contains functions for logging setup.
+
+.. func:: _create_dirs_for_logs(config: dict) -> None
+.. func:: setup_logging(config_path: pathlib.Path, default_level: Union[int, str] = logging.INFO) -> None
+
+.. data:: DEFAULT_LOGGING_BASIC_CONFIG
 """
 
 import logging
@@ -11,18 +16,32 @@ from typing import (
 
 import yaml
 
-from ...settings import CORE_DIR
-
 
 __all__ = ['setup_logging']
 
 
+logger = logging.getLogger(__name__)
+
+
+DEFAULT_LOGGING_BASIC_CONFIG = {
+    "format": "%(asctime)s - %(name)s - %(levelname)s - %(filename)s - %(funcName)s() [%(lineno)s] : %(message)s"
+}
+
+
 def _create_dirs_for_logs(config: dict) -> None:
-    """ """
+    """
+    Create directories for log files.
+
+    :param config: logging config loaded from .yaml file
+    :type config: dict
+
+    :return: None
+    :rtype: None
+    """
+
     for handler_name in config['handlers']:
         if 'file' in handler_name:
-            log_path_from_main_package = config['handlers'][handler_name]['filename']
-            log_path = (CORE_DIR / log_path_from_main_package).resolve()
+            log_path = pathlib.Path(config['handlers'][handler_name]['filename'])
             log_dir_path = log_path.parent
             log_dir_path.mkdir(parents=True, exist_ok=True)
 
@@ -40,6 +59,8 @@ def setup_logging(config_path: pathlib.Path, default_level: Union[int, str] = lo
     :rtype: None
     """
 
+    DEFAULT_LOGGING_BASIC_CONFIG['level'] = default_level
+
     if config_path.exists():
         with open(config_path, 'r') as config_file:
             try:
@@ -49,9 +70,11 @@ def setup_logging(config_path: pathlib.Path, default_level: Union[int, str] = lo
 
                 logging.config.dictConfig(config)
             except Exception as error:
-                # here also might be set default logging
-                raise error
+                logging.basicConfig(**DEFAULT_LOGGING_BASIC_CONFIG)
+                logger.exception(msg='During logging initializing from config file error raised!', exc_info=error)
+                logger.info('Default logging config is using!')
+            else:
+                logger.info('Logging configuration successfully set!')
     else:
-        logging.basicConfig(level=default_level)
-
-        logging.info('Failed to load the configuration file. Default config is using!')
+        logging.basicConfig(**DEFAULT_LOGGING_BASIC_CONFIG)
+        logger.info('During logging initializing failed to load the configuration file. Default config is using!')
