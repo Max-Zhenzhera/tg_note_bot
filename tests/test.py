06b10769af2b9dd867370320170992061da65057
Max Zhenzhera <megashurik@urk.net>
2021-04-29 14:40:29 +0300

    added imports for test.py

--- tests/test.py
+++ tests/test.py
@@ -1,33 +1,37 @@
 """
 Test
 """
-import asyncio
 
-import yarl
-import sqlalchemy as sa
-from sqlalchemy.future import select
-from tg_note_bot.db import models, postgres, db
-from tg_note_bot.db.models import *
-from sqlalchemy.orm import selectinload, joinedload, Query
-from sqlalchemy import engine
-from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy.orm.collections import InstrumentedList
+import asyncio
+import os
 import itertools as it
 import operator
-async_session = postgres.create_db_session()
+
+import dotenv
+import sqlalchemy as sa
+from sqlalchemy.future import select
+from sqlalchemy.orm import (
+    selectinload,
+    joinedload,
+    Query
+)
+from sqlalchemy.ext.asyncio import (
+    AsyncEngine,
+    AsyncSession
+)
+
+from tg_note_bot.db import (
+    models,
+    postgres,
+    db
+)
+
+
+dotenv.load_dotenv()
 
 
 async def main():
-    session: AsyncSession
-    async with async_session() as session:
-        async with session.begin():
-            stmt = select(User).options(selectinload(User.rubrics)).options(selectinload(User.links)).where(User.id == 692001589)
-            r = await session.execute(stmt)
-            a = r.scalar()
-            print(a)
-            print(a.rubrics)
-            print(a.links)
-
+    pass
 
 
 if __name__ == '__main__':
