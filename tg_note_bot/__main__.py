a181ae4f3db6f2b66fc79a298d9065c518e1c828
Max Zhenzhera <megashurik@urk.net>
2021-04-29 14:52:51 +0300

    added package to global path

--- tg_note_bot/__main__.py
+++ tg_note_bot/__main__.py
@@ -2,8 +2,16 @@
 Entry point.
 """
 
-from tg_note_bot.app import main
+import sys
+import pathlib
+
+
+# add package to global path -------------------------------------------------------------------------------------------
+sys.path.append(pathlib.Path(__file__).parent.parent.__str__())
+# ----------------------------------------------------------------------------------------------------------------------
 
 
 if __name__ == '__main__':
+    from tg_note_bot.app import main
+
     main()
